aaae2d2489d2139a9b71e67e8b2e2975aaaab360
Dason-IsopodOverseer <dasonxywang@gmail.com>
2022-05-23 00:14:11 -0700

    dialog map

---
+++ src/dialogSequence/servermap.py
@@ -0,0 +1,90 @@
+import engine.servermap
+import json;
+import os
+
+class ServerMap(engine.servermap.ServerMap):
+    """Extends engine.servermap.ServerMap
+
+    TRIGGER DIALOG MECHANIC
+        Allows cutscenes with text to be shown. Text can be advanced by player input (currently only spacebar at the moment, 
+        I don't know how to get  anything else).
+    
+    FREEZE AND UNFREEZE MECHANIC
+        Set speed of character to zero when needed, and unfreeze if required. Used as part of 
+        dialog cutscenes.
+
+    LOADING DIALOG JSON FILES
+        Loads strings of text stored as a json file
+    """
+
+    # GLOBAL CLASS VARIABLES
+    dialog1 = "empty" #do not alter this under any circumstance
+    inDialog = False
+    dialogCounter = 0
+
+    def initDialogs(self):
+        # find json file
+        dir_name = os.path.dirname(os.path.realpath(__file__))
+        base_filename = "1"
+        folder = "dialog"
+        filename_suffix = "json"
+        parent_path = os.path.join(dir_name, folder)
+        filepath = os.path.join(parent_path, base_filename + "." + filename_suffix)
+        print("Loading dialogs using the following path: " + filepath)
+
+        if os.path.isfile(filepath):
+            print("file found!")
+        else: 
+            print("dialog josn file error.")
+            quit()
+
+        # Opening JSON file
+        with open(filepath) as f:
+            self.dialog1 = json.load(f)
+
+    def freeze(self, sprite):
+        """
+        Change the sprite's moveSpeed to zero.
+        """
+        # if sprite is moving, cancel the movement by setting speed to 0.
+        if "move" in sprite and sprite['move']['type'] == "Linear":
+            sprite['speedMultiNormalSpeed'] = sprite['move']['s']
+            sprite['move']['s'] *= 0
+    
+    def unfreeze(self, sprite):
+        """
+        Reset the sprite's moveSpeed.
+        """
+        if "speedMultiNormalSpeed" in sprite:
+                if "move" in sprite and sprite['move']['type'] == "Linear":
+                    sprite['move']['s'] = sprite['speedMultiNormalSpeed']
+                del sprite['speedMultiNormalSpeed']
+    
+    def triggerSayhello(self, trigger, sprite):
+        self.setSpriteSpeechText(sprite, "I seem to have gotten wet.")
+    
+    def triggerDialog(self, trigger, sprite):
+        if not self.inDialog and (self.dialogCounter == 0):
+            self.freeze(sprite)
+            self.inDialog = True
+            self.speak(sprite)
+            self.dialogCounter += 1
+        elif not self.inDialog and (self.dialogCounter != 0):
+            self.dialogCounter = 0
+            self.unfreeze(sprite)
+        else: 
+            if "action" in sprite:
+                self.speak(sprite)
+                self.dialogCounter += 1          
+
+    """counts the dialog progression and executes dialog.
+    dialog is complete when all lines in the array have been said, so dialogcounter == array.length"""
+    def speak(self, sprite):
+        text = []
+        for i in self.dialog1['1']:
+            text.append(i)
+        if (self.dialogCounter >= len(text)):
+            self.inDialog = False
+            self.delSpriteSpeechText(sprite)
+        else: 
+            self.setSpriteSpeechText(sprite, text[self.dialogCounter])
