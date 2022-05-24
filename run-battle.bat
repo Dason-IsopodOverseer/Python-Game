cf033de8f670b870a0eff1db6401cba204d25aad
emily <emilywang4321@gmail.com>
2022-05-23 23:57:21 -0700

    Co-authored-by: Dason Wang <Dason-IsopodOverseer@users.noreply.github.com> Co-authored-by: GaoZe36 <GaoZe36@users.noreply.github.com>

--- run-battle.bat
+++ run-battle.bat
@@ -2,4 +2,3 @@ CD /d "%~dp0"
 
 start "Server" cmd /K py -3 src/startserver.py -game "battletest" -test
 start "Client" cmd /K py -3 src/startclient.py -game "battletest" -player "Goose"
-
--- src/battletest/servermap.py
+++ src/battletest/servermap.py
@@ -3,3 +3,12 @@ import engine.servermap
 class ServerMap(engine.servermap.ServerMap):
     def triggerSayhello(self, trigger, sprite):
         self.setSpriteSpeechText(sprite, f"Hello World")
+    
+    def triggerAttack(self, trigger, sprite):
+        self.setSpriteSpeechText(sprite, "Press space to attack")
+    
+    def triggerDefend(self, trigger, sprite):
+        self.setSpriteSpeechText(sprite, "Press space to defend")
+    
+    def triggerSpecial(self, trigger, sprite):
+        self.setSpriteSpeechText(sprite, "Press space to use special action")
