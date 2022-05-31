f1091dfed66912d10a7d9b3cbe6fb158edee0dbd
sxile <ejbroc1@gmail.com>
2022-05-30 21:25:34 -0700

    Revert "Merge branch 'main' of https://github.com/Dason-IsopodOverseer/Python-Game"
    This reverts commit afcb3abc16e3802ea48f814fa2250cd02c2e90f6, reversing
    changes made to 85cc416e46cd3da5b6db748303e2648343470fef.

--- run-diag.bat
+++ run-diag.bat
@@ -1,11 +1,5 @@
 CD /d "%~dp0"
 
-<<<<<<< HEAD
 start "Server" cmd /K py -3 src/startserver.py -game "dialogSequence" -test -verbose
 start "Client" cmd /K py -3 src/startclient.py -game "dialogSequence" -player "Character"
-=======
-start "Server" cmd /K py -3 src/startserver.py -game "dialogSequence" -test
-start "Client for Player 1" cmd /K py -3 src/startclient.py -game "dialogSequence" -player "Eric"
-start "Client for Player 2" cmd /K py -3 src/startclient.py -game "dialogSequence" -player "Andre"
->>>>>>> 0615b38d980a7a640dd28af3e91eb290685450c5
 
--- run-mapstest.bat
+++
@@ -1,4 +0,0 @@
-CD /d "%~dp0"
-
-start "Server" cmd /K py -3 src/startserver.py -game "mapstest" -test
-start "Client" cmd /K py -3 src/startclient.py -game "mapstest" -player "Goose"
