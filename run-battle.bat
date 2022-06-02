6f0d42859d316c2a88af347b7bf78fa087ee149c
IsopodOverseer-Dason <dasonxywang@gmail.com>
2022-06-02 11:59:07 -0700

    BIG REVERT WITH EMILY BATTLE

--- run-battle.bat
+++ run-battle.bat
@@ -1,4 +1,6 @@
 CD /d "%~dp0"
 
 start "Server" cmd /K py -3 src/startserver.py -game "battletest" -test
-start "Client" cmd /K py -3 src/startclient.py -game "battletest" -player "Goose"
+start "Client" cmd /K py -3 src/startclient.py -game "battletest" -player "one"
+start "Client" cmd /K py -3 src/startclient.py -game "battletest" -player "two"
+start "Client" cmd /K py -3 src/startclient.py -game "battletest" -player "three"
--- src/battletest/servermap.py
+++ src/battletest/servermap.py
@@ -3,6 +3,7 @@ import engine.servermap
 from engine.log import log
 import engine.stepmap
 import engine.time as time
+import random
 
 class ServerMap(engine.servermap.ServerMap):
     """Extends engine.servermap.ServerMap
@@ -24,6 +25,27 @@ class ServerMap(engine.servermap.ServerMap):
     turnDone = False
     eTurnEndTime = 0
 
+    enemyHealth = 100
+    enemyDmgMult = 1
+    eDefending = False
+    aDefending = False
+    lDefending = False
+    aDmgMult = 1.2
+    eDmgMult = 1
+    lDmgMult = 0.9
+    aDefMult = .75
+    eDefMult = .70
+    lDefMult = .60
+    enAttacked = False
+    currentTurn = 0
+    players = {
+        "Eric" : 30,
+        "Andre" : 45,
+        "Leslie" : 25
+    }
+
+    def getMovability(self):
+        return True
     ########################################################
     # BATTLE ACTION MECHANIC
     ########################################################
@@ -38,12 +60,52 @@ class ServerMap(engine.servermap.ServerMap):
         if "action" in sprite and not self.turnDone:
             self.delSpriteAction(sprite)
             if currentAct == 'a':
-                self.setSpriteSpeechText(sprite, "attacked", time.perf_counter() + 2)
+                damage = random.randrange(8, 12)
+
+                if (sprite["name"] == "Andre"):
+                    damage *= self.aDmgMult
+                elif (sprite["name"] == "Eric"):
+                    damage *= self.eDmgMult
+                elif (sprite["name"] == "Leslie"):
+                    damage *= self.lDmgMult
+
+
+                
+
+
+                n = random.randrange(1, 20)	
+                if (n > 3):	
+                    self.setSpriteSpeechText(sprite, "attacked", time.perf_counter() + 2)
+                    self.enemyHealth -= damage	
+                    if (n == 20):	
+                        self.enemyHealth -= random.randRange(5, 7)	
+                        self.setSpriteSpeechText(sprite, "critical hit!", time.perf_counter() + 1)	
+                else:
+                    self.setSpriteSpeechText(sprite, "oops, that attack whiffed.")
+            	
+            # if player defending
             elif currentAct == 'd':
                 self.setSpriteSpeechText(sprite, "defended", time.perf_counter() + 2)
+                if (sprite["name"] == "Andre"):
+                    self.aDefending = True
+                elif (sprite["name"] == "Eric"):
+                    self.eDefending = True
+                elif (sprite["name"] == "Leslie"):
+                    self.lDefending = True
+
+            # if player using special action
             elif currentAct == 's':
                 self.setSpriteSpeechText(sprite, "acted specially", time.perf_counter() + 2)
-            self.turnDone = True
+
+            self.currentTurn += 1
+            if self.currentTurn > 2:
+                    self.currentTurn = 0
+                    self.turnDone = True
+            while (self.currentTurn == 0 and "Eric" not in self.players) or (self.currentTurn == 1 and "Andre" not in self.players) or (self.currentTurn == 2 and "Leslie" not in self.players):
+                self.currentTurn += 1
+                if self.currentTurn > 2:
+                    self.currentTurn = 0
+                    self.turnDone = True
 
     def triggerAttack(self, trigger, sprite):
         """BATTLE ACTION MECHANIC: triggerAttack method.
@@ -51,17 +113,20 @@ class ServerMap(engine.servermap.ServerMap):
         Prompt player to attack and perform attack if sprite requests action.
         """
         if not self.turnDone:
-            self.setSpriteSpeechText(sprite, "Press space to attack")
-            self.act(self, sprite, 'a')
+            if (self.currentTurn == 0 and sprite["name"] == "Eric") or (self.currentTurn == 1 and sprite["name"] == "Andre") or (self.currentTurn == 2 and sprite["name"] == "Leslie"):
+                self.setSpriteSpeechText(sprite, "Press space to attack")
+                self.act(self, sprite, 'a')
 
+    
     def triggerDefend(self, trigger, sprite):
         """BATTLE ACTION MECHANIC: triggerDefend method.
 
         Prompt player to defend and perform defense if sprite requests action.
         """
         if not self.turnDone:
-            self.setSpriteSpeechText(sprite, "Press space to defend")
-            self.act(self, sprite, 'd')
+            if (self.currentTurn == 0 and sprite["name"] == "Eric") or (self.currentTurn == 1 and sprite["name"] == "Andre") or (self.currentTurn == 2 and sprite["name"] == "Leslie"):
+                self.setSpriteSpeechText(sprite, "Press space to defend")
+                self.act(self, sprite, 'd')
     
     def triggerSpecial(self, trigger, sprite):
         """BATTLE ACTION MECHANIC: triggerSpecial method.
@@ -70,15 +135,18 @@ class ServerMap(engine.servermap.ServerMap):
         requests action.
         """
         if not self.turnDone:
-            self.setSpriteSpeechText(sprite, "Press space to use special action")
-            self.act(self, sprite, 's')
+            if (self.currentTurn == 0 and sprite["name"] == "Eric") or (self.currentTurn == 1 and sprite["name"] == "Andre") or (self.currentTurn == 2 and sprite["name"] == "Leslie"):
+                self.setSpriteSpeechText(sprite, "Press space to use special action")
+                self.act(self, sprite, 's')
     
     ########################################################
     # TURN MECHANIC
     ########################################################
-    def stepMapStartEnemy(self):
+
+    def stepMapStartBattle(self):
         for sprite in self['sprites']:
             if sprite['name'] == "enemy":
+                self.setSpriteLabelText(sprite, "health: " + str(self.enemyHealth))
                 if self.turnDone:
                     #text now working, only disappears if player sprite is moving (?)
                     self.setSpriteSpeechText(sprite, "he he he haw", time.perf_counter() + 0.1)
@@ -86,10 +154,66 @@ class ServerMap(engine.servermap.ServerMap):
                     if self.eTurnEndTime == 0:
                         self.eTurnEndTime = time.perf_counter() + 2
                     elif time.perf_counter() > self.eTurnEndTime:
+
+                        if not self.enAttacked:
+                            x = random.randrange(0, len(self.players)) #determining target of attack
+                            damage = random.randrange(5, 7)*self.enemyDmgMult
+                            target = list(self.players)[x]
+                            if(target == "Eric"): # eric attacked
+                                if self.eDefending:
+                                    damage *= self.eDefMult
+                            elif (target == "Andre"): # andre attacked
+                                if self.aDefending:
+                                    damage *= self.aDefMult
+                            elif (target == "Leslie"): # leslie attacked
+                                if self.lDefending:
+                                    damage *= self.lDefMult
+                            
+                            self.players[target] -= damage
+                            if self.players[target] <= 0:
+                                del self.players[target]
+                            
+                            #log(self.players[target] + " targeted")
+                        
+                        
+                        # reset all the stuff
+                        self.enAttacked = True
                         self.eTurnEndTime = 0
                         self.turnDone = False
+                        self.enAttacked = False
+                        self.eDefending = False
+                        self.aDefending = False
+                        self.lDefending = False
 
-    def getMovability(self):
-        return True
-                       
-                    
+            elif sprite['name'] == "Eric":
+                if "Eric" in self.players:
+                    self.setSpriteLabelText(sprite, "health: " + str(self.players["Eric"]))
+                else:
+                    self.setSpriteLabelText(sprite, "x_x")
+                    self.freeze(sprite)
+            elif sprite['name'] == "Andre":
+                if "Andre" in self.players:
+                    self.setSpriteLabelText(sprite, "Health: " + str(self.players["Andre"]))
+                else:
+                    self.setSpriteLabelText(sprite, "x_x")
+                    self.freeze(sprite)
+            elif sprite['name'] == "Leslie":
+                if "Leslie" in self.players:
+                    self.setSpriteLabelText(sprite, "Health: " + str(self.players["Leslie"]))
+                else:
+                    self.setSpriteLabelText(sprite, "x_x")
+                    self.freeze(sprite)
+
+    def freeze(self, sprite):      
+        #Change the sprite's moveSpeed to zero.
+        # if sprite is moving, cancel the movement by setting speed to 0.
+        if "move" in sprite and sprite['move']['type'] == "Linear":
+            sprite['speedMultiNormalSpeed'] = sprite['move']['s']
+            sprite['move']['s'] *= 0
+ 
+    def unfreeze(self, sprite):
+        #Reset the sprite's moveSpeed.
+        if "speedMultiNormalSpeed" in sprite:
+                if "move" in sprite and sprite['move']['type'] == "Linear":
+                    sprite['move']['s'] = sprite['speedMultiNormalSpeed']
+                del sprite['speedMultiNormalSpeed']
