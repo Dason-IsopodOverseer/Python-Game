d465ea3899818a3e3445f85197c38d1cca5a6f51
IsopodOverseer-Dason <dasonxywang@gmail.com>
2022-06-02 11:33:59 -0700

    removal

--- run-enginetest.bat
+++
@@ -1,5 +0,0 @@
-CD /d "%~dp0"
-
-start "Server" cmd /K py -3 src/startserver.py -game "enginetest" -fps 60 -test -verbose
-start "Client" cmd /K py -3 src/startclient.py -game "enginetest" -fps 60 -player "Bob" -verbose
-
--- run-hello.bat
+++
@@ -1,5 +0,0 @@
-CD /d "%~dp0"
-
-start "Server" cmd /K py -3 src/startserver.py -game "helloworld" -test
-start "Client" cmd /K py -3 src/startclient.py -game "helloworld" -player "Belston"
-
