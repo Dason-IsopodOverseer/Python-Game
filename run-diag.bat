51486c32843224ed66053a1989791f250822668a
Omnificent101 <kmskuter@gmail.com>
2022-06-03 08:50:28 -0700

    .

--- run-diag.bat
+++
@@ -1,6 +0,0 @@
-CD /d "%~dp0"
-
-start "Server" cmd /K py -3 src/startserver.py -game "dialogSequence" -test
-start "Client" cmd /K py -3 src/startclient.py -game "dialogSequence" -player "Character"
-start "Client" cmd /K py -3 src/startclient.py -game "dialogSequence" -player "Character2"
-
