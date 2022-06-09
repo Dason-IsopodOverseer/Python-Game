1927af6b29ecdaad87b58e5e0f8dd73ffb072f9c
IsopodOverseer-Dason <dasonxywang@gmail.com>
2022-06-09 11:35:50 -0700

    removal

--- run-battle.bat
+++
@@ -1,6 +0,0 @@
-CD /d "%~dp0"
-
-start "Server" cmd /K py -3 src/startserver.py -game "battletest" -test
-start "Client" cmd /K py -3 src/startclient.py -game "battletest" -player "one"
-start "Client" cmd /K py -3 src/startclient.py -game "battletest" -player "two"
-start "Client" cmd /K py -3 src/startclient.py -game "battletest" -player "three"
--- run-quick.bat
+++
@@ -1,6 +0,0 @@
-CD /d "%~dp0"
-
-start "Server" cmd /K py -3 src/startserver.py -game "quick" -test
-start "Client" cmd /K py -3 src/startclient.py -game "quick" -player "player1"
-start "Client" cmd /K py -3 src/startclient.py -game "quick" -player "player2"
-start "Client" cmd /K py -3 src/startclient.py -game "quick" -player "player3"
